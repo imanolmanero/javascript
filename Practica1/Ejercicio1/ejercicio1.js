d5a04ddf79370a7a2149fde56c0e2e2c730df765
imanol.manero <imanol.manero.51413@ikasle.egibide.org>
2025-09-24 10:06:31 +0200

    js

---
+++ Practica1/Ejercicio1/ejercicio1.js
@@ -0,0 +1,8 @@
+const usuarios =[
+    {usuario: "Juan", contraseña: 2500},
+    {usuario: "Pedro", contraseña: 1234},
+    {usuario: "Maria", contraseña: 5678},
+    {usuario: "Ana", contraseña: 91011},
+    {usuario: "Luis", contraseña: 1213}
+];
+
